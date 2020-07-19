c8d2dd29b5ded636dd429185d642953d51f50110
Adarsh Lunthi <adarshlunhti09@gmail.com>
2020-07-19 20:50:45 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends Component{
+      constructor(props){
+        super(props);
+        this.state = {
+          isDataPopulated : false,
+          memberids : []
+        }
+      }
+
+      async componentDidMount() {
+        try{
+          const response = await fetch(`https://yayjk.dev/api/members/allIds`);
+          const json = await response.json();
+          this.setState({ memberids: json });
+        }catch(error){
+          console.error();
+        }
+      }
+  
+      
+      render(){
+        return(
+          <div>
+          <ul>
+            {this.state.memberids.map(id => (
+              <li>
+                {id.id}
+              </li>
+            ))}
+          </ul>
+        </div>
+        )
+      } 
+    }
+    
+    
+
 
 export default App;
